e41efe1e396ddb63d1ea6db5450dccb411429e29
funanamy <funanamy@didichuxing.com>
2017-11-09 12:40:37 +0800

    fix input-option, index-list UI bugs

--- example/components/input-option/input-option.vue
+++ example/components/input-option/input-option.vue
@@ -37,20 +37,23 @@
     height: 100%
     justify-content space-between
     align-items stretch
+    transform: rotate(0deg) // fix 子元素超出边框圆角部分不隐藏的问题
+    overflow: hidden
     .name
-      flex: 0 0 auto
-      width: 120px
+      flex: 0 0 120px
       padding-left: 10px
       display inline-flex
       align-items center
     input
       flex: 1 1 auto
-      padding-left: 10px
+      min-width: 1px // fix padding把width撑开的问题
+      padding: 0 10px
       background-color: $color-white
       border-left: 1px solid rgba(0, 0, 0, .1)
       box-shadow: 0 0 1px 1px #eee inset
       outline: none
       &:focus
-        border: 1px solid $color-orange
+        outline: #fc9153 solid 1px;
+        outline-offset: -1px;
 
 </style>
