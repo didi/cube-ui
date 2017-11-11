b8d0fc5f3e2e6c82fa6222dc26f43aae1f41937b
AmyFoxFN <funan_amy@foxmail.com>
2017-11-11 18:30:01 +0800

    add tabs responsive

--- document/components/home/home.vue
+++ document/components/home/home.vue
@@ -2,9 +2,10 @@
   <div class="home-view">
     <div class="navigator">
       <h1 class="logo">
+        <span class="toggle" @click="toggle">=</span>
         <router-link to="/"><span>cube-ui</span></router-link>
       </h1>
-      <div class="tabs">
+      <div class="tabs" :class="{ active: showTabs }">
         <slot name="nav"></slot>
         <site-lang></site-lang>
       </div>
@@ -18,8 +19,18 @@
 <script>
   import Lang from '../lang/lang.vue'
   export default {
+    data() {
+      return {
+        showTabs: false
+      }
+    },
     components: {
       SiteLang: Lang
+    },
+    methods: {
+      toggle() {
+        this.showTabs = !this.showTabs
+      }
     }
   }
 </script>
@@ -27,7 +38,8 @@
   @import "~@/common/stylus/variable.styl"
 
   .home-view
-    min-width: 800px
+    @media screen and (min-width: 1200px)
+      min-width: 800px
     height: 100%
     padding-top: 70px
     box-sizing: border-box
@@ -39,8 +51,16 @@
     margin-top: -70px
     background-color: $color-regular-blue
     .logo
-      float: left
-      background-color: $color-orange
+      @media screen and (min-width: 1200px)
+        float: left
+        background-color: $color-orange
+        .toggle
+          display: none
+      @media screen and (max-width: 1200px)
+        text-align: center
+        .toggle
+          float: left
+          color: #c6c7c8
       a
         display: block
         padding: 0 28px
@@ -56,7 +76,10 @@
           vertical-align: middle
     .tabs
       padding: 0 30px
-      float: right
+      @media screen and (min-width: 1200px)
+        float: right
+      @media screen and (max-width: 1200px)
+        display: none
       .tab
         display: inline-block
         color: $color-white
@@ -66,4 +89,9 @@
           color: $color-orange
       .router-link-active
         color: $color-orange
+
+      &.active
+        @media screen and (max-width: 1200px)
+          display: block
+          height: 200px
 </style>
