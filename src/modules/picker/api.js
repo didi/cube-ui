dac6196cd73b5377846892bbc72455323c38ea32
dolymood <dolymood@gmail.com>
2017-11-28 18:50:06 +0800

    Picker singleton warn

--- src/modules/picker/api.js
+++ src/modules/picker/api.js
@@ -1,5 +1,11 @@
 import createAPI from '../../common/helpers/create-api'
+import { warn } from '../../common/helpers/debug'
 
 export default function addPicker (Vue, Picker) {
-  createAPI(Vue, Picker, ['select', 'value-change', 'cancel', 'change'], false)
+  const pickerAPI = createAPI(Vue, Picker, ['select', 'value-change', 'cancel', 'change'], false)
+  pickerAPI.before((data, renderFn, single) => {
+    if (single) {
+      warn('Picker component can not be a singleton.')
+    }
+  })
 }
