21c237499c80179b6e9cd3b6f1afcc9812d3792d
dolymood <dolymood@gmail.com>
2017-11-28 18:50:13 +0800

    TimePicker singleton warn

--- src/modules/time-picker/api.js
+++ src/modules/time-picker/api.js
@@ -1,5 +1,11 @@
 import createAPI from '../../common/helpers/create-api'
+import { warn } from '../../common/helpers/debug'
 
 export default function addTimePicker (Vue, TimePicker) {
-  createAPI(Vue, TimePicker, ['select', 'cancel', 'change'], false)
+  const timePickerAPI = createAPI(Vue, TimePicker, ['select', 'cancel', 'change'], false)
+  timePickerAPI.before((data, renderFn, single) => {
+    if (single) {
+      warn('TimePicker component can not be a singleton.')
+    }
+  })
 }
