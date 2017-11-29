8708a4d98e03f849e19e87612aa719088fe7dccd
dolymood <dolymood@gmail.com>
2017-11-29 17:19:05 +0800

    update picker & time-picker createAPI params

--- src/modules/picker/api.js
+++ src/modules/picker/api.js
@@ -2,7 +2,7 @@ import createAPI from '../../common/helpers/create-api'
 import { warn } from '../../common/helpers/debug'
 
 export default function addPicker (Vue, Picker) {
-  const pickerAPI = createAPI(Vue, Picker, ['select', 'value-change', 'cancel', 'change'], false)
+  const pickerAPI = createAPI(Vue, Picker, ['select', 'value-change', 'cancel', 'change'])
   pickerAPI.before((data, renderFn, single) => {
     if (single) {
       warn('Picker component can not be a singleton.')
--- src/modules/time-picker/api.js
+++ src/modules/time-picker/api.js
@@ -2,7 +2,7 @@ import createAPI from '../../common/helpers/create-api'
 import { warn } from '../../common/helpers/debug'
 
 export default function addTimePicker (Vue, TimePicker) {
-  const timePickerAPI = createAPI(Vue, TimePicker, ['select', 'cancel', 'change'], false)
+  const timePickerAPI = createAPI(Vue, TimePicker, ['select', 'cancel', 'change'])
   timePickerAPI.before((data, renderFn, single) => {
     if (single) {
       warn('TimePicker component can not be a singleton.')
