692f3ecf75d6e227686083a2e915bf01cf639778
dolymood <dolymood@gmail.com>
2017-12-21 15:09:53 +0800

    update(example): ref dir

--- example/main.js
+++ example/main.js
@@ -4,104 +4,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Cube from '../src/index'
 import App from './App.vue'
-
-import Button from './pages/button.vue'
-import Checkbox from './pages/checkbox.vue'
-import CheckboxGroup from './pages/checkbox-group.vue'
-import Loading from './pages/loading.vue'
-import Tip from './pages/tip.vue'
-import Popup from './pages/popup.vue'
-import Toast from './pages/toast.vue'
-import Picker from './pages/picker.vue'
-import CascadePicker from './pages/cascade-picker.vue'
-import TimePicker from './pages/time-picker.vue'
-import Dialog from './pages/dialog.vue'
-import ActionSheet from './pages/action-sheet.vue'
-import Scroll from './pages/scroll.vue'
-import Slide from './pages/slide.vue'
-import IndexList from './pages/index-list.vue'
-import IndexListDefault from './pages/index-list-default.vue'
-import IndexListCustom from './pages/index-list-custom.vue'
+import router from './router'
 
 Vue.use(Cube)
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/button',
-    component: Button
-  },
-  {
-    path: '/checkbox',
-    component: Checkbox
-  },
-  {
-    path: '/checkbox-group',
-    component: CheckboxGroup
-  },
-  {
-    path: '/loading',
-    component: Loading
-  },
-  {
-    path: '/tip',
-    component: Tip
-  },
-  {
-    path: '/popup',
-    component: Popup
-  },
-  {
-    path: '/toast',
-    component: Toast
-  },
-  {
-    path: '/picker',
-    component: Picker
-  },
-  {
-    path: '/cascade-picker',
-    component: CascadePicker
-  },
-  {
-    path: '/time-picker',
-    component: TimePicker
-  },
-  {
-    path: '/dialog',
-    component: Dialog
-  },
-  {
-    path: '/action-sheet',
-    component: ActionSheet
-  },
-  {
-    path: '/scroll',
-    component: Scroll
-  },
-  {
-    path: '/slide',
-    component: Slide
-  },
-  {
-    path: '/index-list',
-    component: IndexList,
-    children: [
-      {
-        path: 'default',
-        component: IndexListDefault
-      },
-      {
-        path: 'custom',
-        component: IndexListCustom
-      }
-    ]
-  }
-]
-
-const router = new VueRouter({
-  routes
-})
 
 /* eslint-disable no-new */
 new Vue({
