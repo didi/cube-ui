963ea5fc3f28a38401e2b8f59b364f3fe3a129ef
dolymood <dolymood@gmail.com>
2018-01-03 15:01:37 +0800

    support nav loading, fix issue #64

--- document/common/js/utils.js
+++ document/common/js/utils.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import cookie from 'js-cookie'
 
 export function setItem(k, v) {
@@ -32,4 +33,7 @@ export function getCurrentLang() {
 }
 
 const baseUrl = process.env.NODE_ENV === 'production' ? 'https://didi.github.io/cube-ui/example/#/' : `http://${window.location.hostname}:8081/#/`
-export { baseUrl }
+
+const eventHub = new Vue()
+
+export { baseUrl, eventHub }
---
+++ document/components/nav-loading/nav-loading.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="nav-loading" :class="loadingCls"></div>
+</template>
+
+<script>
+  import { eventHub } from '../../common/js/utils'
+
+  export default {
+    data() {
+      return {
+        loading: false,
+        toEnd: false
+      }
+    },
+    computed: {
+      loadingCls() {
+        const cls = {}
+        if (this.loading) {
+          cls['nav-loading-ani'] = true
+        }
+        if (this.toEnd) {
+          cls['nav-loading-ani-end'] = true
+        }
+        return cls
+      }
+    },
+    created() {
+      let tid
+      let startTime
+      eventHub.$on('begin-loading', () => {
+        startTime = Date.now()
+        clearTimeout(tid)
+        this.loading = true
+      })
+      eventHub.$on('finish-loading', () => {
+        const time = Date.now() - startTime
+        if (time > 500) {
+          this.resetLoading()
+        } else {
+          tid = setTimeout(this.resetLoading, 500 - time)
+        }
+      })
+    },
+    methods: {
+      resetLoading() {
+        this.toEnd = true
+        setTimeout(() => {
+          this.toEnd = false
+          this.loading = false
+        }, 100)
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @require "~@/common/stylus/variable.styl"
+
+  .nav-loading
+    z-index: 10
+    position: absolute
+    width: 100%
+    height: 2px
+    background: #009a61
+    transform: translateX(-100%)
+  .nav-loading-ani
+    animation: navloading 2s linear forwards
+  .nav-loading-ani-end
+    animation: navloadingend .1s linear
+  @keyframes navloading
+    0%
+      opacity: 0
+      transform: translateX(-100%)
+    20%
+      opacity: .5
+      transform: translateX(-60%)
+    50%
+      opacity: .8
+      transform: translateX(-40%)
+    80%
+      opacity: .8
+      transform: translateX(-20%)
+    100%
+      opacity: .8
+      transform: translateX(-10%)
+  @keyframes navloadingend
+    0%
+      opacity: .8
+      transform: translateX(-10%)
+    100%
+      opacity: 0
+      transform: translateX(0)
+</style>
