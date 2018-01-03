7e05b0ab5076b2b4d71f5c146ab0605fdf025f20
dolymood <dolymood@gmail.com>
2018-01-03 17:52:10 +0800

    update(build): relase publish registry

--- build/release/publish.sh
+++ build/release/publish.sh
@@ -11,7 +11,7 @@ git push origin master
 # replace src/ __VERSION__
 node ./build/release/replace-version.js
 # publish
-npm publish
+npm publish --registry=https://registry.npmjs.org
 # checkout src/index.js
 git checkout src/index.js
 git checkout dev
