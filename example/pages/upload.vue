31c0c24a31c9a57fef1c40ad1808cef5a4fd2686
dolymood <dolymood@gmail.com>
2018-01-16 19:46:36 +0800

    update(example): upload add compress upload

--- example/pages/upload.vue
+++ example/pages/upload.vue
@@ -1,21 +1,38 @@
 <template>
   <cube-page type="upload-view" title="Upload">
     <template slot="content">
-      <cube-upload ref="upload" :action="action" :simultaneous-uploads="1"></cube-upload>
-      <cube-button @click="upload" v-if="!isUploading">Upload</cube-button>
-      <cube-button @click="pause" v-else>Pause</cube-button>
-      <cube-button @click="retry">Retry</cube-button>
+      <p>Normal upload: </p>
+      <div>
+        <cube-upload ref="upload" :action="action" :simultaneous-uploads="1"></cube-upload>
+        <cube-button @click="upload" v-if="!isUploading">Upload</cube-button>
+        <cube-button @click="pause" v-else>Pause</cube-button>
+        <cube-button @click="retry">Retry</cube-button>
+      </div>
+      <p>Compress&Base64 upload: </p>
+      <div>
+        <cube-upload
+          ref="upload2"
+          :action="action2"
+          :simultaneous-uploads="1"
+          :parse-file="parseFile"
+          @file-submitted="fileSubmitted"></cube-upload>
+      </div>
     </template>
   </cube-page>
 </template>
 
 <script type="text/ecmascript-6">
   import CubePage from '../components/cube-page.vue'
+  import compress from '../modules/image'
 
   export default {
     data() {
       return {
         action: '//jsonplaceholder.typicode.com/photos/',
+        action2: {
+          target: '//jsonplaceholder.typicode.com/photos/',
+          prop: 'base64Value'
+        },
         isUploading: true
       }
     },
@@ -30,6 +47,18 @@
       },
       retry() {
         this.$refs.upload.retry()
+      },
+      parseFile(file, next) {
+        compress(file, {
+          compress: {
+            width: 1600,
+            height: 1600,
+            quality: 0.5
+          }
+        }, next)
+      },
+      fileSubmitted(file) {
+        file.base64Value = file.file.base64
       }
     },
     components: {
@@ -42,6 +71,9 @@
   .upload-view
     .content
       >
-        *
+        p, div
           margin: 20px 0
+        div
+          .cube-btn, .cube-upload
+            margin: 10px 0
 </style>
