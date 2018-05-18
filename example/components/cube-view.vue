17caf068720d330a4d3813a93a6d132ded3f57b7
tank0317 <fengweiyao0317@hotmail.com>
2018-05-18 15:22:03 +0800

    Scroll: muti examples
    * [update] scroll example, split old scroll to two pages
    
    * [add]{heizontal.vue} heizontal scroll demo
    
    * [add] fake jd home page scroll example
    
    * [add] fake toutiao scroll example
    
    * [fix] replace cube-view translate3d to translate

--- example/components/cube-view.vue
+++ example/components/cube-view.vue
@@ -10,9 +10,9 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
   .cube-view
-    transform: translate3d(0, 0, 0)
+    transform: translate(0, 0)
   .page-move-enter, .page-move-leave-active
-    transform: translate3d(100%, 0, 0)
+    transform: translate(100%, 0)
   .page-move-enter-active, .page-move-leave-active
     transition: transform .3s
 </style>
---
+++ example/pages/scroll/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <cube-page type="scroll" title="Scroll">
+    <div slot="content">
+      <cube-button-group>
+        <cube-button @click="goTo('default')">Default</cube-button>
+        <cube-button @click="goTo('config')">Config</cube-button>
+        <cube-button @click="goTo('herizontal')">Herizontal</cube-button>
+        <cube-button @click="goTo('jd')">京东</cube-button>
+        <cube-button @click="goTo('toutiao')">头条</cube-button>
+      </cube-button-group>
+      <cube-view></cube-view>
+    </div>
+  </cube-page>
+</template>
+
+<script type="text/ecmascript-6">
+  import CubePage from 'example/components/cube-page.vue'
+  import CubeButtonGroup from 'example/components/cube-button-group.vue'
+  import CubeView from 'example/components/cube-view.vue'
+
+  export default {
+    components: {
+      CubePage,
+      CubeButtonGroup,
+      CubeView
+    },
+    methods: {
+      goTo(subPath) {
+        this.$router.push('/scroll/' + subPath)
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
