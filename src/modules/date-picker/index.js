50ac0140f7dc4174728f5b2086605ba5b734b52a
dolymood <dolymood@gmail.com>
2018-05-18 12:20:56 +0800

    components relations

--- src/modules/date-picker/index.js
+++ src/modules/date-picker/index.js
@@ -1,15 +1,21 @@
+import Picker from '../../components/picker/picker.vue'
 import CascadePicker from '../../components/cascade-picker/cascade-picker.vue'
 import DatePicker from '../../components/date-picker/date-picker.vue'
 import addDatePicker from './api'
 import addCascadePicker from '../cascade-picker/api'
+import addPicker from '../picker/api'
 
 DatePicker.install = function (Vue) {
+  Vue.component(Picker.name, Picker)
   Vue.component(CascadePicker.name, CascadePicker)
   Vue.component(DatePicker.name, DatePicker)
+  addPicker(Vue, Picker)
   addCascadePicker(Vue, CascadePicker)
   addDatePicker(Vue, DatePicker)
 }
 
+DatePicker.Picker = Picker
+CascadePicker.Picker = Picker
 DatePicker.CascadePicker = CascadePicker
 
 export default DatePicker
--- src/modules/form/index.js
+++ src/modules/form/index.js
@@ -2,13 +2,23 @@ import Form from '../../components/form/form.vue'
 import FormGroup from '../../components/form/form-group.vue'
 import FormItem from '../../components/form/form-item.vue'
 
+import Validator from '../../components/validator/validator.vue'
+import { addRule, addMessage, addType, setLanguage } from '../../common/helpers/validator'
+
 Form.install = function (Vue) {
   Vue.component(Form.name, Form)
   Vue.component(FormGroup.name, FormGroup)
   Vue.component(FormItem.name, FormItem)
+  Vue.component(Validator.name, Validator)
 }
 
 Form.Group = FormGroup
 Form.Item = FormItem
+Form.Validator = Validator
+
+Validator.addRule = addRule
+Validator.addMessage = addMessage
+Validator.addType = addType
+Validator.setLanguage = setLanguage
 
 export default Form
