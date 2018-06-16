fa366a405cd4e442a314267ea1018b6a9dc3df13
dolymood <dolymood@gmail.com>
2018-06-16 23:28:58 +0800

    refactor(scroll-nav-bar): init demo

---
+++ example/pages/scroll-nav-bar.vue
@@ -0,0 +1,50 @@
+<template>
+  <cube-page type="scroll-nav-bar" title="ScrollNavBar">
+    <div slot="content">
+      <cube-scroll-nav-bar :current="current" :labels="labels" @change="changeHandler" />
+      <div class="side-container">
+        <cube-scroll-nav-bar direction="vertical" :current="current" :labels="labels" @change="changeHandler">
+          <i slot-scope="props">{{props.txt}}</i>
+        </cube-scroll-nav-bar>
+      </div>
+    </div>
+  </cube-page>
+</template>
+
+<script type="text/ecmascript-6">
+  import CubePage from '../components/cube-page.vue'
+
+  export default {
+    data() {
+      return {
+        current: '快车',
+        labels: [
+          '快车',
+          '小巴',
+          '专车',
+          '顺风车',
+          '代驾',
+          '公交',
+          '自驾租车',
+          '豪华车',
+          '二手车',
+          '出租车'
+        ]
+      }
+    },
+    methods: {
+      changeHandler(cur) {
+        this.current = cur
+      }
+    },
+    components: {
+      CubePage
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.side-container
+  height: 300px
+  margin-top: 20px
+</style>
