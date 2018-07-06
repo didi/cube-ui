46eb6686b6ce5c4cde3df3ca0e4aeb23d7360db7
dolymood <dolymood@gmail.com>
2018-07-06 17:39:19 +0800

    chore(example): fix #213

--- example/components/cube-view.vue
+++ example/components/cube-view.vue
@@ -9,8 +9,6 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  .cube-view
-    transform: translate(0, 0)
   .page-move-enter, .page-move-leave-active
     transform: translate(100%, 0)
   .page-move-enter-active, .page-move-leave-active
