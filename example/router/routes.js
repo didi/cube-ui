a461a726b5d1437e3d108086c2f002cbefa39d3b
qinhongna <qinhongna@didichuxing.com>
2018-10-10 15:21:57 +0800

    refactor(demo): add checker

--- example/router/routes.js
+++ example/router/routes.js
@@ -65,6 +65,7 @@ import Tab from '../pages/tab-bar/tab-entry.vue'
 import TabBasic from '../pages/tab-bar/tab-basic.vue'
 import TabComposite from '../pages/tab-bar/tab-composite.vue'
 import ScrollTab from '../pages/tab-bar/scroll-tab.vue'
+import Checker from '../pages/checker/checker.vue'
 
 const routes = [
   {
@@ -78,6 +79,10 @@ const routes = [
   {
     path: '/checkbox-group',
     component: CheckboxGroup
+  },
+  {
+    path: '/checker',
+    component: Checker
   },
   {
     path: '/radio',
