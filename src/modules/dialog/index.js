1d8c109fb62433cba1d263cc8f174126d48998a1
dolymood <dolymood@gmail.com>
2018-10-10 16:48:30 +0800

    feat(dialog): support type prompt
    #298

--- src/modules/dialog/index.js
+++ src/modules/dialog/index.js
@@ -1,9 +1,13 @@
 import Dialog from '../../components/dialog/dialog.vue'
+import Input from '../../components/input/input.vue'
 import addDialog from './api'
 
 Dialog.install = function (Vue) {
+  Vue.component(Input.name, Input)
   Vue.component(Dialog.name, Dialog)
   addDialog(Vue, Dialog)
 }
+
+Dialog.Input = Input
 
 export default Dialog
