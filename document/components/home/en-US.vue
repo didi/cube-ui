41ca8c3bec8c25de1bba0d13e6a04409fe63f59f
dolymood <dolymood@gmail.com>
2018-11-15 17:58:32 +0800

    docs(nav): add video tutorial

--- document/components/home/en-US.vue
+++ document/components/home/en-US.vue
@@ -3,6 +3,7 @@
     <template slot="nav">
       <router-link to="/en-US/docs" class="tab"><span>Docs</span></router-link>
       <router-link to="/en-US/example" class="tab"><span>Example</span></router-link>
+      <a href="https://coding.imooc.com/class/74.html" target="_blank" class="tab">Video Tutorial</a>
     </template>
     <div slot="home">home</div>
   </home>
