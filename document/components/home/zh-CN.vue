19b4c5365778c2e7b0848d95d5aa63c8cf464bed
theniceangel <theniceangel@163.com>
2019-01-08 15:21:02 +0800

    chore(docs): remove Video Tutorial

--- document/components/home/zh-CN.vue
+++ document/components/home/zh-CN.vue
@@ -3,7 +3,6 @@
     <template slot="nav">
       <router-link to="/zh-CN/docs" class="tab"><span>文档</span></router-link>
       <router-link to="/zh-CN/example" class="tab"><span>示例</span></router-link>
-      <a href="https://coding.imooc.com/class/74.html" target="_blank" class="tab">视频教程</a>
     </template>
     <div slot="home">home</div>
   </home>
