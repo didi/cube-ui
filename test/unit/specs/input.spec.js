e9a23dbba0c650b9ddeaefe0de10721847e5de6b
dolymood <dolymood@gmail.com>
2019-04-18 19:37:17 +0800

    test(input): fix click to touchevent

--- test/unit/specs/input.spec.js
+++ test/unit/specs/input.spec.js
@@ -1,7 +1,7 @@
 import Vue from 'vue2'
 import Input from '@/modules/input'
 import createVue from '../utils/create-vue'
-import { createEvent } from '../utils/event'
+import { createEvent, dispatchTap } from '../utils/event'
 
 describe('Input.vue', () => {
   let vm
@@ -38,7 +38,7 @@ describe('Input.vue', () => {
     vm = createInput(1)
     expect(vm.$el.querySelector('input').value)
       .is.not.empty
-    vm.$el.querySelector('.cube-input-clear').click()
+    dispatchTap(vm.$el.querySelector('.cube-input-clear'))
     setTimeout(() => {
       expect(vm.$el.querySelector('input').value)
         .is.empty
