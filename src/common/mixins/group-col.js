a5fe6b43f0d8ef25214a30c9d41bd003a4e0bd25
QiuShuiBai <1125097005@qq.com>
2019-08-07 08:36:32 +0800

    feat(checkbox-group): add a col configuration

---
+++ src/common/mixins/group-col.js
@@ -0,0 +1,26 @@
+import { prefixStyle } from '../helpers/dom'
+const FLEXBASIS = prefixStyle('flexBasis')
+
+export default {
+  props: {
+    colNum: {
+      type: Number,
+      default: 1
+    }
+  },
+  watch: {
+    // Merge the watch handlers of data and selectedIndex into one.
+    colNum: {
+      immediate: true,
+      handler (newValue) {
+        if (newValue <= 1) return ''
+        this.$nextTick(() => {
+          let count = 100 / newValue + '%'
+          Array.prototype.forEach.call(this.$refs.group.children, item => {
+            item.style[FLEXBASIS] = count
+          })
+        })
+      }
+    }
+  }
+}
