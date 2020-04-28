7f5d5234e6aedf2d3da684e6cf71beae8dfbdf86
zhouchaozczc <zhouchaozczc@didiglobal.com>
2020-04-28 16:22:57 +0800

    feat(helpers): add a variable to determine whether it is iOS environment

--- src/common/helpers/env.js
+++ src/common/helpers/env.js
@@ -2,3 +2,4 @@
 export const inBrowser = typeof window !== 'undefined'
 export const ua = inBrowser && navigator.userAgent.toLowerCase()
 export const isAndroid = ua && ua.indexOf('android') > 0
+export const isIOS = ua && /iphone|ipad|ipod|ios/.test(ua)
