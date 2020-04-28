0840740d45b984769eaea65528e8f987e4cd345a
zhouchaozczc <zhouchaozczc@didiglobal.com>
2020-04-28 16:29:26 +0800

    feat(bscroll): set the default value of variable use-transition

---
+++ src/common/bscroll/constants.js
@@ -0,0 +1,18 @@
+import { ua, isIOS } from '../helpers/env'
+import { getIOSVersion } from '../helpers/util'
+
+function getUseTransition() {
+  let useTransition = true
+  if (isIOS) {
+    const version = getIOSVersion(ua)
+    if (!version) return useTransition
+
+    if (version.major >= 13 && version.minor >= 4) {
+      useTransition = false
+    }
+  }
+  return useTransition
+}
+
+// fix the scrolling problem in iOS13.4 webview
+export const USE_TRANSITION = getUseTransition()
