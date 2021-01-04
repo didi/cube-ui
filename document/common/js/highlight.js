b710059f6a093fffda33b2146854f663fc73510e
zhouchaozczc <zhouchaozczc@didiglobal.com>
2021-01-04 14:24:37 +0800

    chore(site): fix path err (#745)

--- document/common/js/highlight.js
+++ document/common/js/highlight.js
@@ -1,4 +1,4 @@
-import hljs from 'highlight.js/lib/highlight'
+import hljs from 'highlight.js/lib/core'
 
 import shell from 'highlight.js/lib/languages/shell'
 import css from 'highlight.js/lib/languages/css'
