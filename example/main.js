20080c70e89d2dd1f68efb2ec8f7374a320b4a33
zouhang <1102639563@qq.com>
2022-12-21 11:05:31 +0800

    chore: update icon.style

--- example/main.js
+++ example/main.js
@@ -3,4 +3,6 @@ import Cube from '../src/index'
 import App from './App.vue'
 import router from './router'
 
+import '../src/common/icon/cube-icon.styl'
+
 createApp(App).use(Cube).use(router).mount('#app')
