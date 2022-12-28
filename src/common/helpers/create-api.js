b4e25315dee1d144318b488ae5f72f743593d5a1
zouhang <1102639563@qq.com>
2022-12-28 17:39:30 +0800

    chore: update create api logic

--- src/common/helpers/create-api.js
+++ src/common/helpers/create-api.js
@@ -6,7 +6,6 @@ const instances = []
 const createComponent = (componentCtor, options) => {
   const container = document ? document.createElement('div') : null
   const id = 'cube_create_component_' + seed++
-
   const vm = createVNode(componentCtor, {
     ...options,
     id
@@ -19,13 +18,21 @@ const createComponent = (componentCtor, options) => {
     document.body.appendChild(container)
   }
 
-  console.log(vm, vm.component)
   return vm
 }
 
 export default function createAPI(app, Component, events, single) {
   app.config.globalProperties[`$create${Component.name.replace('cube-', '').replace(/^\w/, ($) => $.toUpperCase())}`] = function(options) {
     if (single && Component._instance) {
+      if (options) {
+        // reset default value
+        Object.keys(Component.props).forEach(key => {
+          Component._instance.component.props[key] = Component.props[key].default
+        })
+        Object.keys(options).forEach(key => {
+          Component._instance.component.props[key] = options[key]
+        })
+      }
       return Component._instance.component.proxy
     }
     const vm = Component._instance = createComponent(Component, options)
