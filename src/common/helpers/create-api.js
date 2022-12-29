80c27fd16daaa0236c20bce62a81034a2484dc1d
zouhang <1102639563@qq.com>
2022-12-29 11:36:42 +0800

    chore: update create-api

--- src/common/helpers/create-api.js
+++ src/common/helpers/create-api.js
@@ -1,4 +1,4 @@
-import { render, createVNode } from 'vue'
+import { render, createVNode, mergeProps } from 'vue'
 
 let seed = 0
 const instances = []
@@ -14,7 +14,30 @@ const createComponent = (componentCtor, options) => {
   instances.push(vm)
 
   if (container) {
+    // mounted component
     render(vm, container)
+
+    // add remove vm
+    vm.component.proxy['$remove'] = function(cb) {
+      render(null, container)
+      cb && cb()
+      if (container && document.body.contains(container)) {
+        document.body.removeChild(container)
+      }
+    }
+
+    // add $updateProps
+    vm.component.proxy['$updateProps'] = function(props) {
+      // reset default value
+      Object.keys(componentCtor.props).forEach(key => {
+        vm.component.props[key] = componentCtor.props[key].default
+      })
+      // set new props
+      Object.keys(props).forEach(key => {
+        vm.component.props[key] = props[key]
+      })
+    }
+
     document.body.appendChild(container)
   }
 
@@ -25,17 +48,21 @@ export default function createAPI(app, Component, events, single) {
   app.config.globalProperties[`$create${Component.name.replace('cube-', '').replace(/^\w/, ($) => $.toUpperCase())}`] = function(options) {
     if (single && Component._instance) {
       if (options) {
-        // reset default value
-        Object.keys(Component.props).forEach(key => {
-          Component._instance.component.props[key] = Component.props[key].default
-        })
-        Object.keys(options).forEach(key => {
-          Component._instance.component.props[key] = options[key]
-        })
+        Component._instance.component.proxy.$updateProps(options)
       }
       return Component._instance.component.proxy
     }
     const vm = Component._instance = createComponent(Component, options)
+
+    const parentVnodeProps = this ? this._.vnode.props : null
+    if (parentVnodeProps) {
+      this._.vnode.props = mergeProps(parentVnodeProps || {}, {
+        onVnodeBeforeUnmount() {
+          vm.component.proxy.$remove()
+        }
+      })
+    }
+
     return vm.component.proxy
   }
 }
