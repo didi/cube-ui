3a392dc6798d3ad4fe23f649009e86df0a85eb66
zouhang <1102639563@qq.com>
2023-01-05 16:28:52 +0800

    chore: update router-view with transition

--- example/components/cube-view.vue
+++ example/components/cube-view.vue
@@ -1,7 +1,9 @@
 <template>
-  <transition name="page-move">
-    <router-view class="cube-view"></router-view>
-  </transition>
+  <router-view v-slot="{ Component }">
+    <transition name="page-move">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <script>
